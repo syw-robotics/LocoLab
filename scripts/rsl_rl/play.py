f9c9ed8c7f3459f3ef15c7908140514628679bec
haoxiang liang <wizardhxliang@gmail.com>
2026-01-20 22:20:08 +0800

    add g1 urdf

--- scripts/rsl_rl/play.py
+++ scripts/rsl_rl/play.py
@@ -96,7 +96,7 @@ from isaaclab.envs import (
 )
 from isaaclab.utils.assets import retrieve_file_path
 from isaaclab.utils.dict import print_dict
-from isaaclab.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
+from isaaclab_rl.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
 from isaaclab_tasks.utils import get_checkpoint_path
 from isaaclab_tasks.utils.hydra import hydra_task_config
 
